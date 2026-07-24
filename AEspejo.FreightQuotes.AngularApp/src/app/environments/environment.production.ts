852cadd0a516c8d1a4c73fbcda127449eeb76097
Alvaro Espejo <alvaro.espejo@hotmail.com>
2026-07-24 11:00:41 -0400

    Update CI/CD

--- AEspejo.FreightQuotes.AngularApp/src/app/environments/environment.production.ts
+++ AEspejo.FreightQuotes.AngularApp/src/app/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-api-produccion.com/api'
+  apiUrl: 'https://freightquotesapi-dxc8d0agaqepd0c4.canadacentral-01.azurewebsites.net/api'
 };
